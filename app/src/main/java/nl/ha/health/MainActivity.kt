875313f3a1213d1afe1ddb2907a2c150a690607b
MarziruMe <maartenmentink@gmail.com>
2026-06-27 03:10:58 +0200

    Create MainActivity.kt

---
+++ app/src/main/java/nl/ha/health/MainActivity.kt
@@ -0,0 +1,15 @@
+package nl.ha.health
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        startActivity(Intent(this, SettingsActivity::class.java))
+        finish()
+    }
+}
